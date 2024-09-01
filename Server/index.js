e0946c3a0e66a3a9cd8cb6a7d382c69f987101df
Timothy <pastims@gmail.com>
2024-09-01 22:28:46 +0100

    Adjusted base urls

--- Server/index.js
+++ Server/index.js
@@ -9,9 +9,8 @@ import { SchoolSupervisorRouter } from "./Routes/SchoolSupervisorRoute.js";
 import { IndustrySupervisorRouter } from "./Routes/IndustrySupervisorRoute.js";
 
 const app = express() 
-const apiUrl = import.meta.env.VITE_API_BASE_URL;
 app.use(cors({
-    origin: [apiUrl],
+    origin: ["https://siwes-e-logbook-2024.vercel.app/"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
