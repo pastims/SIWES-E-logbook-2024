e054ccc26109f01d2788e726b049e347616023ff
Timothy <pastims@gmail.com>
2024-09-02 23:11:13 +0100

    Adjusted some more

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,17 +1,17 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const con = new Pool({
-    host: "dpg-cr9ugnaj1k6c73bm5tmg-a",
-    user: "siwes_elogbook_user",
-    password: "Bq0YonioWQr3u0VaqzRTNMgMGqYPcioe",
-    port: 5432,
-    database: "siwes_elogbook",
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+// const con = new Pool({
+//     host: "dpg-cr9ugnaj1k6c73bm5tmg-a",
+//     user: "siwes_elogbook_user",
+//     password: "Bq0YonioWQr3u0VaqzRTNMgMGqYPcioe",
+//     port: 5432,
+//     database: "siwes_elogbook",
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
 
-// import mysql from 'mysql'
+import mysql from 'mysql'
 
 // const con = mysql.createConnection({
 //     host: "sql.freedb.tech",
@@ -20,19 +20,19 @@ const con = new Pool({
 //     database: "freedb_siwes-elogbook"
 // })
 
-// const con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "siwes-elogbook"
-// })
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "siwes-elogbook"
+})
 
 con.connect(function(err) {
     if(err) {
         console.log("Conection Error", err.stack)
     } else {
         // console.log("Connected!")
-        console.log("Connected to PostgreSQL!")
+        console.log("Connected to Database")
     }
 })
 
